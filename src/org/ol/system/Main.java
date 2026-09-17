ad50441a46443c7eacccd08532d59246fffa1f1c
octavio letona <oletona-2026097@kinal.edu.gt>
2026-09-17 09:14:07 -0600

    feat: Agregamos la estructura mvc + dao

--- src/org/ol/system/Main.java
+++ src/org/ol/system/Main.java
@@ -11,7 +11,8 @@ package org.ol.system;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+ 
+        System.out.println("hola mundo");
     }
     
 }
