3709a4ca95742de2f68cd3c87343c331f70b70a8
octavio-letona <oletona-2026097@kinal.edu.gt>
2026-09-18 07:22:36 -0600

    feat(model): Agregamos la clase Iibro.java y sus atributos encapsulados

---
+++ src/org/ol/model/Libro.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.ol.model;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String fechaPublicacion;
+    private double precio;
+    private int idCategoria;
+    private String nitEditorial;
+    private int stock;
+
+    public Libro() {
+    }
+
+    public Libro(String isbn, String titulo, String fechaPublicacion, double precio, int idCategoria, String nitEditorial, int stock) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.fechaPublicacion = fechaPublicacion;
+        this.precio = precio;
+        this.idCategoria = idCategoria;
+        this.nitEditorial = nitEditorial;
+        this.stock = stock;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(String fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNitEditorial() {
+        return nitEditorial;
+    }
+
+    public void setNitEditorial(String nitEditorial) {
+        this.nitEditorial = nitEditorial;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
+}
