3da261e251dec50c4c590adee48bb88649542134
octavio letona <oletona-2026097@kinal.edu.gt>
2026-09-24 09:31:06 -0600

    feat(javadoc): Agregamos el javadoc de AuortDAO

--- src/org/ol/dao/AutorDAO.java
+++ src/org/ol/dao/AutorDAO.java
@@ -1,12 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package org.ol.dao;
 
 import org.ol.model.Autor;
 
+/**
+ * Contrato DAO que define las operaciones de persistencia CRUD
+ * para la entidad {@link Autor} dentro del sistema.
+ * 
+ * Extiende de {@link Crud} empleando un identificador de tipo {@link Integer}
+ * (ID único del autor) como clave primaria.
+ * 
+ * @author Octavio Letona
+ * @version 1.0.0
+ * @see org.ol.model.Autor
+ * @see org.ol.dao.Crud
+ */
+public interface AutorDAO extends Crud<Autor, Integer> {
 
-public interface AutorDAO extends Crud<Autor, Integer>{
-    
 }
