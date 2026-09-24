ff0b10ba9404e99e4971eb577f82baf0720c7423
octavio letona <oletona-2026097@kinal.edu.gt>
2026-09-24 09:27:50 -0600

    feat(javadoc): Agregamos el javadoc de categoriaDAO

--- src/org/ol/dao/CategoriaDAO.java
+++ src/org/ol/dao/CategoriaDAO.java
@@ -1,16 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package org.ol.dao;
-
 
 import org.ol.model.Categoria;
 
 /**
- *
- * @author aruba
+ * Contrato DAO que define las operaciones de persistencia CRUD
+ * para la entidad {@link Categoria} dentro del sistema.
+ * 
+ * Extiende de {@link Crud} empleando un identificador de tipo {@link Integer}
+ * (ID único de la categoría) como clave primaria.
+ * 
+ * @author Octavio Letona
+ * @version 1.0.0
+ * @see org.ol.model.Categoria
+ * @see org.ol.dao.Crud
  */
-public interface CategoriaDAO extends Crud<Categoria, Integer>{
-    
+public interface CategoriaDAO extends Crud<Categoria, Integer> {
+
 }
