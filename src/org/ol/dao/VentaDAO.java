da06e68b2d6e91251cd1ff548fefb5dc0d2ebe08
octavio letona <oletona-2026097@kinal.edu.gt>
2026-09-24 08:45:46 -0600

    feat(dao): Agregamos el dao y el daoimpl de venta

---
+++ src/org/ol/dao/VentaDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.ol.dao;
+
+import java.util.List;
+import org.ol.model.LineaVenta;
+import org.ol.model.Venta;
+
+public interface VentaDAO extends Crud<Venta, Integer>{
+    //crearVenta inserta el encabezado de la venta, sus líneas y descuenta el stock.
+    //Devuelve el no_venta generado (o -1 si falla).
+    int crearVenta(Venta venta, List<LineaVenta> lineas);
+}
