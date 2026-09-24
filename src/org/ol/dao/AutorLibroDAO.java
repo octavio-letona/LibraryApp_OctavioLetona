9409d501f0dc66465e163b9a31e60ceea78b3b7d
octavio letona <oletona-2026097@kinal.edu.gt>
2026-09-24 09:28:26 -0600

    feat(javadoc): Agregamos el javadoc de categoriaDAO

--- src/org/ol/dao/AutorLibroDAO.java
+++ src/org/ol/dao/AutorLibroDAO.java
@@ -1,11 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package org.ol.dao;
 
 import org.ol.model.AutorLibro;
 
-public interface AutorLibroDAO extends Crud<AutorLibro, Integer>{
-    
+/**
+ * Contrato DAO que define las operaciones de persistencia CRUD
+ * para la entidad {@link AutorLibro} dentro del sistema.
+ * 
+ * Extiende de {@link Crud} empleando un identificador de tipo {@link Integer}
+ * (ID único de la relación autor-libro) como clave primaria.
+ * 
+ * @author Octavio Letona
+ * @version 1.0.0
+ * @see org.ol.model.AutorLibro
+ * @see org.ol.dao.Crud
+ */
+public interface AutorLibroDAO extends Crud<AutorLibro, Integer> {
+
 }
